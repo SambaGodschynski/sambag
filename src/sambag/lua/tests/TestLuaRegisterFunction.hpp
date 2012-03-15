b778da04983f440b986df1e47ad78f17a64b4191
samba <samba@localubuntu>
2012-03-15 23:17:35 +0100

    register function with max 5 f. args implemented

--- src/sambag/lua/tests/TestLuaRegisterFunction.hpp
+++ src/sambag/lua/tests/TestLuaRegisterFunction.hpp
@@ -17,20 +17,20 @@ class TestLuaScript : public CPPUNIT_NS::TestFixture {
 private:
 	CPPUNIT_TEST_SUITE( TestLuaScript );
 	CPPUNIT_TEST( testRegisterFunction );
-	//CPPUNIT_TEST( testRegisterFunction02 );
-	//CPPUNIT_TEST( testRegisterFunction03 );
-	//CPPUNIT_TEST( testRegisterFunction04 );
-	//CPPUNIT_TEST( testRegisterFunction05 );
+	CPPUNIT_TEST( testRegisterFunction02 );
+	CPPUNIT_TEST( testRegisterFunction03 );
+	CPPUNIT_TEST( testRegisterFunction04 );
+	CPPUNIT_TEST( testRegisterFunction05 );
 	CPPUNIT_TEST_SUITE_END();
 	lua_State *L;
 public:
 	virtual void setUp();
 	virtual void tearDown();
 	void testRegisterFunction();
-	//void testRegisterFunction02();
-	//void testRegisterFunction03();
-	//void testRegisterFunction04();
-	//void testRegisterFunction05();
+	void testRegisterFunction02();
+	void testRegisterFunction03();
+	void testRegisterFunction04();
+	void testRegisterFunction05();
 };
 } // namespace
 #endif /* TESTLUAREGISTERFUNCTION_H_ */
