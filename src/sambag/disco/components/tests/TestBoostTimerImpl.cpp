f17cea43f85440287662c74dac0d982d913bb425
acer <samba@detleff.(none)>
2012-08-09 12:35:52 +0200

    TestBoostTimerImpl added

---
+++ src/sambag/disco/components/tests/TestBoostTimerImpl.cpp
@@ -0,0 +1,16 @@
+/*
+ * TestBoostTimerImpl.cpp
+ *
+ *  Created on: Thu Aug  9 12:35:18 2012
+ *      Author: Johannes Unger
+ */
+
+#include "TestBoostTimerImpl.hpp"
+
+namespace sambag { 
+//=============================================================================
+//  Class TestBoostTimerImpl
+//=============================================================================
+//-----------------------------------------------------------------------------
+
+} // namespace(s)
---
+++ src/sambag/disco/components/tests/TestBoostTimerImpl.hpp
@@ -0,0 +1,30 @@
+/*
+ * TestBoostTimerImpl.hpp
+ *
+ *  Created on: Thu Aug  9 12:35:18 2012
+ *      Author: Johannes Unger
+ */
+
+#ifndef SAMBAG_TESTBOOSTTIMERIMPL_H
+#define SAMBAG_TESTBOOSTTIMERIMPL_H
+
+#include <boost/shared_ptr.hpp>
+
+namespace sambag {
+
+//=============================================================================
+/** 
+  * @class TestBoostTimerImpl.
+  */
+class TestBoostTimerImpl {
+//=============================================================================
+public:
+	//-------------------------------------------------------------------------
+	typedef boost::shared_ptr<TestBoostTimerImpl> Ptr;
+protected:
+private:
+public:
+}; // TestBoostTimerImpl
+} // namespace(s)
+
+#endif /* SAMBAG_TESTBOOSTTIMERIMPL_H */
