6c79728a9c1e4a38276767396243c5d0f048e517
samba <samba@mainframe>
2012-12-13 18:03:23 +0100

    IEditor added

---
+++ src/sambag/dsp/IEditor.hpp
@@ -0,0 +1,23 @@
+/*
+ * IEditor.hpp
+ *
+ *  Created on: Wed Dec 12 11:34:17 2012
+ *      Author: Johannes Unger
+ */
+
+#ifndef SAMBAG_IEDITOR_H
+#define SAMBAG_IEDITOR_H
+
+namespace sambag { namespace dsp { 
+//=============================================================================
+/** 
+ * @classe IEditor
+ * Editor Base.
+ */
+struct IEditor {
+//=============================================================================
+	virtual ~IEditor() {}
+};	
+}} // namespace(s)
+
+#endif /* SAMBAG_IEDITOR_H */
