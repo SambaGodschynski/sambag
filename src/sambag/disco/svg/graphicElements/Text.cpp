c2e4cf29e1119434d825925da28bb8093b0cd4ab
Johannes Unger <mac@mini>
2014-08-15 18:02:54 +0200

    &nbsp replaced with _

--- src/sambag/disco/svg/graphicElements/Text.cpp
+++ src/sambag/disco/svg/graphicElements/Text.cpp
@@ -7,6 +7,8 @@
 
 #include "Text.hpp"
 #include "sambag/com/Common.hpp"
+#include <boost/algorithm/string.hpp>
+
 namespace sambag { namespace disco { namespace svg { namespace graphicElements {
 
 //=============================================================================
@@ -23,5 +25,9 @@ void Text::shape( IDrawContext::Ptr cn ) {
 	cn->moveTo(pos.solve(cn));
     cn->textPath(text);
 }
-
+//-----------------------------------------------------------------------------
+void Text::setText(const std::string &t)  {
+    text = t;
+    boost::algorithm::replace_all(text, "nbsp;", " ");
+}
 }}}}
